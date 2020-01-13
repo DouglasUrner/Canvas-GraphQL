b92a470a76a9f6ab5820602803a5e2f6294a2cdb
Douglas Urner <dlu@canishe.com>
2020-01-12 22:08:39 -0800

    Basic query running.

--- canvas-graphql.rb
+++ canvas-graphql.rb
@@ -1,12 +1,21 @@
+#!/usr/bin/env ruby
+
+require "graphql"
 require "graphql/client"
 require "graphql/client/http"
+require "json"
 
 module CGQL
   # Configure GraphQL endpoint using the basic HTTP network adapter.
   HTTP = GraphQL::Client::HTTP.new("https://canvas.instructure.com/api/graphql") do
     def headers(context)
       # Optionally set any HTTP headers
-      { "User-Agent": "ClassStatus" }
+      {
+        "Accept": "application/json",
+        "Authorization": "Bearer #{ENV['ACCESS_TOKEN']}",
+        "Content-Type": "application/json",
+        "User-Agent": "Ruby/graphql-client"
+      }
     end
   end
 
@@ -21,4 +30,50 @@ module CGQL
   # Schema = GraphQL::Client.load_schema("path/to/schema.json")
 
   Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+end
+
+CourseListQuery = CGQL::Client.parse <<-'GRAPHQL'
+query {
+  allCourses {
+    id
+    name
+  }
+}
+GRAPHQL
+
+CourseQuery = CGQL::Client.parse <<-'GRAPHQL'
+query {
+  course(id: "1692944") {
+    name
+    modulesConnection {
+      nodes {
+        name
+      }
+    }
+  }
+}
+GRAPHQL
+
+def query(definition, variables = {})
+  response = CGQL::Client.query(
+    definition,
+    variables: variables,
+    context: client_context
+  )
+
+  case response
+  when GraphQL::Client::Response
+    response.data
+  when GraphQL::Client::FailedResponse
+    raise response.errors
+  end
+end
+
+def client_context
+  { access_token: ENV['ACCESS_TOKEN'] }
+end
+
+data = query(CourseQuery)
+data.course.modules_connection.nodes.each do |node|
+  puts node.name
 end
